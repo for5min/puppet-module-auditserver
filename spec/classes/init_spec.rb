36227f0e2030c3a9a08bff3a770d7f3fe12045a5
Lei Tu <alex.lei.tu@gmail.com>
2013-11-27 16:46:25 +0800

    Update init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -8,23 +8,23 @@ describe 'auditserver' do
     
     it {
       should contain_file('auditserver_pl').with({
-        :path    => '/usr/bin/auditserver.pl',
-        :owner   => 'root',
-        :group   => 'root',
-        :mode    => '755',
-        :content => "template('auditserver/auditEISserver.erb')",
+        'path'    => '/usr/bin/auditserver.pl',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '755',
+        'content' => "template('auditserver/auditEISserver.erb')",
       })
     }
 
 
     it {
       should contain_cron('auditserver_run').with({
-        :command  => "/usr/bin/auditserver.pl cnsh",
-        :user     => 'root',
-        :month    => '11',
-        :monthday => '22',
-        :hour     => '12',
-        :minute   => '0',
+        'command'  => "/usr/bin/auditserver.pl cnsh",
+        'user'     => 'root',
+        'month'    => '11',
+        'monthday' => '22',
+        'hour'     => '12',
+        'minute'   => '0',
       })
     }
     
